8bb108185b3b6219833781c8da6541d79f516165
yan <yan@mit.edu>
2016-11-18 15:58:40 -0800

    use timestamps + randombytes for nonce

--- test/crypto.js
+++ test/crypto.js
@@ -4,50 +4,47 @@ const assert = require('assert')
 const crypto = require('../lib/crypto')
 
 function byteArrayToString (byteArray) {
-  return Array.from(byteArray).join(' ')
+  return byteArray.join(' ')
 }
 
-function pad (arr) {
-  // Pads an array to length 24
-  while (arr.length < 24) {
-    arr.push(0)
-  }
-  return arr
-}
+const zeroes = '0 0 0 0 0 0 0 0 0 0 0 0 0'
 
-function arrayToNonceString (arr) {
-  return pad(arr).join(' ')
-}
+let previousNonces = []
 
 describe('getNonce', () => {
-  it('gets a nonce with deviceID 0 and 0 messages', () => {
-    assert.equal(arrayToNonceString([0, 1]),
-      byteArrayToString(crypto.getNonce(0, 0)))
+  it('gets a nonce with deviceID 0', () => {
+    let nonce = crypto.getNonce(0)
+    previousNonces.push(byteArrayToString(nonce))
+    assert.equal(nonce.length, 24)
+    assert.equal(nonce[0], 0)
+    assert.equal(nonce[5], 1)
+    assert(nonce[6] > 87)
+    assert.equal(zeroes, byteArrayToString(nonce.slice(11)))
   })
-  it('gets a nonce with deviceID 1 and 0 messages', () => {
-    assert.equal(arrayToNonceString([1, 1]),
-      byteArrayToString(crypto.getNonce(1, 0)))
+  it('gets a nonce with deviceID 244', () => {
+    let nonce = crypto.getNonce(244)
+    previousNonces.push(byteArrayToString(nonce))
+    assert.equal(nonce.length, 24)
+    assert.equal(nonce[0], 244)
+    assert.equal(nonce[5], 1)
+    assert(nonce[6] > 87)
+    assert.equal(zeroes, byteArrayToString(nonce.slice(11)))
   })
-  it('gets a nonce with 1-byte message count', () => {
-    assert.equal(arrayToNonceString([1, 1, 255]),
-      byteArrayToString(crypto.getNonce(1, 255)))
+  it('does not repeat nonces', () => {
+    var i = 0
+    while (i < 100) {
+      i++
+      let nonce = crypto.getNonce(1)
+      console.log('got nonce', byteArrayToString(nonce))
+      assert.equal(nonce[0], 1)
+      assert.equal(nonce[5], 1)
+      assert(nonce[6] > 87)
+      assert.equal(zeroes, byteArrayToString(nonce.slice(11)))
+      assert(!previousNonces.includes(byteArrayToString(nonce)))
+    }
   })
-  it('gets nonces with multi-byte message count', () => {
-    assert.equal(arrayToNonceString([2, 2, 1, 0]),
-      byteArrayToString(crypto.getNonce(2, 256)))
-    assert.equal(arrayToNonceString([2, 3, 1, 0, 1]),
-      byteArrayToString(crypto.getNonce(2, 256 * 256 + 1)))
-    assert.equal(arrayToNonceString([2, 4, 1, 0, 3, 3]),
-      byteArrayToString(crypto.getNonce(2, 256 * 256 * 256 + 3 * 256 + 3)))
-    assert.equal(arrayToNonceString([1, 22, 1]),
-      byteArrayToString(crypto.getNonce(1, Math.pow(256, 21))))
-    assert.equal(arrayToNonceString([1, 22, 1, 2]),
-      byteArrayToString(crypto.getNonce(1, Math.pow(256, 21) + 2 * Math.pow(256, 20))))
-  })
-  it('throws if message count or device id is too high', () => {
+  it('throws if device id is too high', () => {
     assert.throws(
       crypto.getNonce.bind(null, 256, 1), /Invalid device/)
-    assert.throws(
-      crypto.getNonce.bind(null, 1, Math.pow(256, 22)), /Nonce count/)
   })
 })
