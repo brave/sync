a0247c25ba990bcab1fb9d724d2a72fa5eadd1f8
yan <yan@mit.edu>
2016-11-18 17:23:39 -0800

    add encoder/decoder and tests

--- test/crypto.js
+++ test/crypto.js
@@ -47,3 +47,24 @@ describe('getNonce', () => {
       crypto.getNonce.bind(null, 256, 1), /Invalid device/)
   })
 })
+
+describe('encrypt and decrypt', () => {
+  const key = new Uint8Array([149, 180, 182, 164, 238, 114, 52, 28, 87, 253, 230, 254, 239, 174, 160, 156, 180, 174, 143, 196, 59, 87, 148, 212, 179, 123, 187, 239, 251, 38, 96, 60])
+  const message = '€ are my favorite moneys'
+  it('encrypted data has the correct length', () => {
+    const result = crypto.encrypt(new Uint8Array([]), key, 0)
+    assert.equal(result.ciphertext.length, 16)
+    assert.equal(result.nonce.length, 24)
+    const result2 = crypto.encrypt(new Uint8Array(Array(128)), key, 0)
+    assert.equal(result2.ciphertext.length, 144)
+    assert.equal(result2.nonce.length, 24)
+  })
+  it('encodes and decodes between byte array and string', () => {
+    assert.equal(message, crypto.byteArrayToString(crypto.stringToByteArray(message)))
+  })
+  it('decrypts to original message', () => {
+    const encrypted = crypto.encrypt(crypto.stringToByteArray(message), key, 0)
+    const decrypted = crypto.decrypt(encrypted.ciphertext, encrypted.nonce, key)
+    assert.equal(crypto.stringToByteArray(message).join(' '), decrypted.join(' '))
+  })
+})
