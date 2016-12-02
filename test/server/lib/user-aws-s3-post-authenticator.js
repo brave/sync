3eb51d1acec27dbc4d5709d3a54f8dad31f98c80
Ayumi Yu <ayumi@ayumiyu.com>
2016-12-01 19:25:25 -0800

    s3 post authenticator: add test for expired signature

--- test/server/lib/user-aws-s3-post-authenticator.js
+++ test/server/lib/user-aws-s3-post-authenticator.js
@@ -3,6 +3,7 @@ const awsSdk = require('aws-sdk')
 const config = require('config')
 const crypto = require('../../../lib/crypto')
 const request = require('request')
+const timekeeper = require('timekeeper')
 const util = require('../../../server/lib/util.js')
 
 const UserAwsS3PostAuthenticator = require('../../../server/lib/user-aws-s3-post-authenticator.js')
@@ -34,7 +35,7 @@ test('userAwsS3PostAuthenticator', (t) => {
     t.equals(result.bucket, config.awsS3Bucket, 'returns S3 bucket')
     t.assert(result.postParams, 'returns post params')
 
-    t.test('uploading sync records works', (t) => {
+    t.test('works: uploading sync records', (t) => {
       const objectKey = `${apiVersion}/${userId}/${categoryIdHistorySites}/1234/objectData`
       const formData = Object.assign(
         {},
@@ -62,7 +63,33 @@ test('userAwsS3PostAuthenticator', (t) => {
       t.end()
     })
 
-    t.test('uploading to another user\'s prefix fails', (t) => {
+    t.test('deny: uploading sync records with expired signed params', (t) => {
+      timekeeper.freeze(new Date().getTime() - config.userAwsCredentialTtl * 1000)
+      const result = new UserAwsS3PostAuthenticator(userId).perform()
+      timekeeper.reset()
+
+      const objectKey = `${apiVersion}/${userId}/${categoryIdHistorySites}/1234/objectData`
+      const formData = Object.assign(
+        {},
+        { key: objectKey },
+        result.postParams,
+        { file: new Buffer([]) }
+      )
+      request.post({
+        url: util.awsS3Endpoint(),
+        formData: formData
+      }, (_error, response, body) => {
+        if (response.statusCode >= 200 && response.statusCode <= 299) {
+          t.fail(`${t.name} (${response.statusCode}) (${body})`)
+        } else {
+          t.pass(t.name)
+        }
+      })
+
+      t.end()
+    })
+
+    t.test('deny: uploading to another user\'s prefix', (t) => {
       const keysTwo = crypto.deriveKeys(crypto.getSeed())
       const userIdTwo = Buffer.from(keysTwo.publicKey).toString('base64')
       const objectKey = `${apiVersion}/${userIdTwo}/${categoryIdHistorySites}/1234/objectData`
@@ -85,7 +112,7 @@ test('userAwsS3PostAuthenticator', (t) => {
       t.end()
     })
 
-    t.test('uploading objects with content fails', (t) => {
+    t.test('deny: uploading objects with content', (t) => {
       const objectKey = `${apiVersion}/${userId}/${categoryIdHistorySites}/1234/objectData`
       const formData = Object.assign(
         {},
