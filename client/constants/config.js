58d7e632044e9bcaee57f867063a91fd94d336d3
yan <yan@mit.edu>
2016-12-07 00:39:43 +0000

    start sync client
    this initializes the sync client and authenticates it using the local test server.
    
    auditors: @ayumi
    
    test plan:
    1. npm run buildtest
    2. in a separate terminal, run `python -m SimpleHTTPServer`
    3. go to localhost:8000/client.html in a browser, open the console, it should say 'successfully authenticated'

---
+++ client/constants/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  serverHost: null,
+  // Change this to whatever address 'npm start' reports
+  testHost: 'http://localhost:4000'
+}
---
+++ client/init.js
@@ -0,0 +1,43 @@
+// @flow
+'use strict'
+
+const crypto = require('../lib/crypto')
+const messages = require('./constants/messages')
+
+/**
+ * Initializes crypto and device ID
+ * @param {Object} chrome window.chrome object or stub
+ * @returns {Promise}
+ */
+module.exports.init = function (chrome/* : Object */) {
+  return new Promise((resolve, reject) => {
+    if (!chrome || !chrome.ipc) {
+      reject('Browser does not support chrome.ipc')
+      return
+    }
+    chrome.ipc.send(messages.GET_INIT_DATA)
+    chrome.ipc.on(messages.GOT_INIT_DATA, (e, seed, deviceId) => {
+      if (seed === null) {
+        // Generate a new "persona"
+        seed = crypto.getSeed()
+        deviceId = new Uint8Array([0])
+        chrome.ipc.send(messages.SAVE_INIT_DATA, seed, deviceId)
+        // XXX: The background process must listen for SAVE_INIT_DATA and emit
+        // GOT_INIT_DATA once the save is successful
+        return
+      }
+      if (!(seed instanceof Uint8Array) || seed.length !== crypto.SEED_SIZE) {
+        reject('Invalid crypto seed')
+      }
+      resolve({keys: crypto.deriveKeys(seed), deviceId})
+    })
+  })
+}
+
+/**
+ * Gets/sets the device ID
+ * @param {Object} chrome window.chrome object or stub
+ */
+module.exports.getDeviceId = function (chrome/* : Object */) {
+  // TODO
+}
