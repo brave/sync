e128b7c83b5b167dcb329a0a33c2ad1ff564c40b
yan <yan@mit.edu>
2016-12-09 22:38:21 +0000

    rename buildtest command, use remote staging server

--- client/constants/config.js
+++ client/constants/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Change this to whatever address 'npm start' reports for testing
-  serverOrigin: 'http://localhost:4000',
+  serverOrigin: 'https://sync-staging.brave.com',
   // Set this to the origin where client.html is loaded
   clientOrigin: 'http://localhost:8000'
 }
