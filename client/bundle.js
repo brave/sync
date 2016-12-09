8f6a44132fe0dd676f9a1ee60d5d52f3cd0794b7
yan <yan@mit.edu>
2016-12-09 23:15:28 +0000

    make client state variables private

--- client/bundle.js
+++ client/bundle.js
@@ -104,10 +104,6 @@ const config = require('./constants/config')
 const serializer = require('../lib/serializer')
 const crypto = require('../lib/crypto')
 
-window.deviceId = null
-window.userId = null
-window.keys = {}
-
 // logging
 const DEBUG = 0
 const WARN = 1
@@ -115,6 +111,9 @@ const ERROR = 2
 const logElement = document.querySelector('#output')
 
 var clientSerializer = null
+var clientDeviceId = null
+var clientUserId = null
+var clientKeys = {}
 
 /**
  * Logs stuff on the visible HTML page.
@@ -140,29 +139,45 @@ const getAWSCredentials = () => {
   if (clientSerializer === null) {
     throw new Error('Serializer not initialized.')
   }
-  const request = new window.Request(`${serverUrl}/${window.userId}/credentials`, {
+  const request = new window.Request(`${serverUrl}/${clientUserId}/credentials`, {
     method: 'POST',
-    body: crypto.sign(clientSerializer.stringToByteArray(now), window.keys.secretKey)
+    body: crypto.sign(clientSerializer.stringToByteArray(now), clientKeys.secretKey)
   })
   return window.fetch(request)
+}
+
+/**
+ * Sets the device ID if one does not yet exist.
+ * @returns {Promise}
+ */
+const maybeSetDeviceId = () => {
+  if (clientDeviceId !== null) {
+    return
+  }
+}
+
+/**
+ * Starts the sync process.
+ */
+const startSync = () => {
 }
 
 Promise.all([serializer.init(''), initializer.init(window.chrome)]).then((values) => {
   clientSerializer = values[0]
   const keys = values[1].keys
   const deviceId = values[1].deviceId
-  window.keys = keys
+  clientKeys = keys
   if (deviceId instanceof Uint8Array && deviceId.length === 1) {
-    window.deviceId = deviceId
+    clientDeviceId = deviceId
     logSync(`initialized deviceId ${deviceId[0]}`)
   }
   if (keys.publicKey instanceof Uint8Array) {
-    window.userId = window.encodeURIComponent(window.btoa(String.fromCharCode.apply(null, keys.publicKey)))
+    clientUserId = window.encodeURIComponent(window.btoa(String.fromCharCode.apply(null, keys.publicKey)))
   }
-  if (!window.userId || !window.keys.secretKey) {
+  if (!clientUserId || !clientKeys.secretKey) {
     throw new Error('Missing userID or keys')
   }
-  logSync(`initialized userId ${window.userId}`)
+  logSync(`initialized userId ${clientUserId}`)
 })
   .catch((e) => { logSync('could not init sync: ' + e, ERROR) })
   .then(() => {
@@ -173,10 +188,15 @@ Promise.all([serializer.init(''), initializer.init(window.chrome)]).then((values
       throw new Error('server response ' + response.status)
     }
   })
-  .then(() => {
-    logSync('successfully authenticated userId ' + window.userId)
-  })
   .catch((e) => { logSync('could not get AWS credentials: ' + e, ERROR) })
+  .then(() => {
+    logSync('successfully authenticated userId ' + clientUserId)
+    return maybeSetDeviceId()
+  })
+  .catch((e) => { logSync('could not register device ID ' + e, ERROR) })
+  .then(() => {
+    startSync()
+  })
 
 },{"../lib/crypto":5,"../lib/serializer":6,"./constants/config":1,"./init":3}],5:[function(require,module,exports){
 // @flow
