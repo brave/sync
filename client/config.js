96c20981c94ebf54c603e2a8546ba46ac6137a0d
yan <yan@mit.edu>
2016-12-13 01:42:06 +0000

    update master to use sync.brave.com

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Set this to the sync server origin
-  serverOrigin: 'https://sync-staging.brave.com',
+  serverOrigin: 'https://sync.brave.com',
   // Set this to the origin where client.html is loaded
   clientOrigin: 'http://localhost:8000'
 }
