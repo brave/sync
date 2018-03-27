ee955a2e611ebb89238888c222896e142c3993b7
yan <yan@mit.edu>
2018-03-26 17:41:04 -0700

    update to sync v1.4.3

--- client/config.js
+++ client/config.js
@@ -20,5 +20,5 @@ module.exports = {
   // 2-byte encryption nonce counter, rotated periodically
   nonceCounter: 0,
   // Sync library version, updated every brave/sync release
-  syncVersion: 'v1.4.2'
+  syncVersion: 'v1.4.3'
 }
