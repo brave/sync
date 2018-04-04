b76ab975d9b65be3a4458be503033d005a7790dd
yan <yan@mit.edu>
2018-04-04 13:33:53 -0700

    fix lint errors with new version of standard

--- client/config.js
+++ client/config.js
@@ -14,9 +14,9 @@ module.exports = {
   // Maximum amount of messages to receive from SQS
   SQS_MAX_LIST_MESSAGES_COUNT: 10,
   // How many seconds messages will be invisible for another poll
-  SQS_MESSAGES_VISIBILITY_TIMEOUT: 30,  // In seconds
+  SQS_MESSAGES_VISIBILITY_TIMEOUT: 30, // In seconds
   // How many seconds we should wait for messages
-  SQS_MESSAGES_LONGPOLL_TIMEOUT: 3,  // In seconds
+  SQS_MESSAGES_LONGPOLL_TIMEOUT: 3, // In seconds
   // 2-byte encryption nonce counter, rotated periodically
   nonceCounter: 0,
   // Sync library version, updated every brave/sync release
